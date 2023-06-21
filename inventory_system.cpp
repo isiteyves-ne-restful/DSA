d6b68fffc6a743905ed90fb7cb27f67f00e3e086
IsiteYves <yvesisite@gmail.com>
2023-06-21 10:16:01 +0200

    ft-completed all required requirement specifications

--- inventory_system.cpp
+++ inventory_system.cpp
@@ -73,9 +73,19 @@ public:
     // Function to list items in alphabetical order
     void listItems()
     {
+        // Check if there are any items and display  message if not
+        if (items.empty())
+        {
+            cout << "No items are recorded yet." << endl;
+            return;
+        }
+
+        // OTHERWISE (IF THERE ARE ITEMS)
+        // Sort items in alphabetical order
         sort(items.begin(), items.end(), [](const Item &a, const Item &b)
              { return a.getItemName() < b.getItemName(); });
 
+        // Display items looping through the vector
         for (const auto &item : items)
         {
             cout << item.toString() << endl;
@@ -128,7 +138,7 @@ public:
                 }
             }
             file.close();
-            cout << "Items loaded successfully!" << endl;
+            cout << "Stored Items loaded successfully!" << endl;
         }
         else
         {
@@ -137,7 +147,107 @@ public:
     }
 };
 
+// Function to clear the console screen
+void clearScreen()
+{
+#ifdef _WIN32
+    system("cls"); // For Windows
+#else
+    system("clear"); // For Linux and macOS
+#endif
+}
+
+// Function to display help
+void displayHelp()
+{
+    cout << "--------------------------------------\n";
+    cout << "*       Commands syntaxes              *\n";
+    cout << "--------------------------------------\n";
+    cout << "itemadd <item_id> <item_name> <quantity> <registration_date>\n";
+    cout << "itemslist\n";
+    cout << "help\n";
+    cout << "exit\n";
+}
+
+// Function to convert a string to lowercase
+string toLowercase(const string &str)
+{
+    string lowercaseStr = str;
+    transform(lowercaseStr.begin(), lowercaseStr.end(), lowercaseStr.begin(), ::tolower);
+    return lowercaseStr;
+}
+
 int main()
 {
+    // create an inventory object instance
+    Inventory inventory("items.csv");
+
+    string command;
+    cout << "--------------------------------------" << endl;
+    cout << "*          INVENTORY SYSTEM            *" << endl;
+    cout << "--------------------------------------" << endl;
+
+    // load items from the file and tell the user when they are ready
+    inventory.loadItems();
+
+    // display help about supported commands and their syntaxes
+    displayHelp();
+
+    while (true)
+    {
+        cout << "\nEnter a command> ";
+        getline(cin, command);
+
+        // Convert the command to lowercase
+        command = toLowercase(command);
+
+        // handle different cases according to the entered command
+        if (command == "itemadd")
+        {
+            int id, quantity;
+            string name, regDate;
+
+            cout << "Enter item ID: ";
+            cin >> id;
+            cin.ignore();
+
+            cout << "Enter item name: ";
+            getline(cin, name);
+
+            cout << "Enter item quantity: ";
+            cin >> quantity;
+            cin.ignore();
+
+            cout << "Enter item registration date: ";
+            getline(cin, regDate);
+
+            inventory.addItem(id, name, quantity, regDate);
+            inventory.saveItems();
+
+            cout << "Item added successfully!\n";
+        }
+        else if (command == "itemslist")
+        {
+            inventory.listItems();
+        }
+        else if (command == "help")
+        {
+            displayHelp();
+        }
+        else if (command == "clear" || command == "cls")
+        {
+            clearScreen(); // Clear the console screen
+        }
+        else if (command == "exit")
+        {
+            cout << "Exiting the program...\n";
+            break;
+        }
+        else
+        {
+            cout << "Invalid command. Please try again.\n";
+        }
+    }
+
     return 0;
 }
